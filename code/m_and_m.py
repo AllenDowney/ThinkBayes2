e9665145482b0ff29ef0020038abe36bc877c3db
Allen Downey <downey@allendowney.com>
2015-12-01 14:42:48 -0500

    Updating m_and_m.py

--- code/m_and_m.py
+++ code/m_and_m.py
@@ -18,14 +18,16 @@ class M_and_M(Suite):
                  red=20,
                  green=10,
                  orange=10,
-                 tan=10)
+                 tan=10,
+                 blue=0)
 
     mix96 = dict(blue=24,
                  green=20,
                  orange=16,
                  yellow=14,
                  red=13,
-                 brown=13)
+                 brown=13,
+                 tan=0)
 
     hypoA = dict(bag1=mix94, bag2=mix96)
     hypoB = dict(bag1=mix96, bag2=mix94)
