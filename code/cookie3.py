c84c46ce8bf23859646d568f2aaec84b89d2516c
Allen Downey <downey@allendowney.com>
2015-12-29 14:13:09 -0500

    Updating cookie3.py

--- code/cookie3.py
+++ code/cookie3.py
@@ -11,33 +11,45 @@ import thinkbayes2
 
 
 class Cookie(thinkbayes2.Suite):
-    """A map from string bowl ID to probablity."""
+    """Suite to represent bowls of cookies."""
 
     def Likelihood(self, data, hypo):
         """The likelihood of the data under the hypothesis.
 
         data: string cookie type
-        hypo: string bowl ID
+        hypo: Hist of cookies
         """
+        # compute the likelihood with the current bowls
         like = hypo[data] / hypo.Total()
+
+        # update the bowl
         if like:
             hypo[data] -= 1
+
         return like
 
 
 def main():
+    # use Hists to represent the contents of the bowls
     bowl1 = thinkbayes2.Hist(dict(vanilla=30, chocolate=10))
     bowl2 = thinkbayes2.Hist(dict(vanilla=20, chocolate=20))
-    pmf = Cookie([bowl1, bowl2])
+
+    # instantiate the suite
+    suite = Cookie([bowl1, bowl2])
 
     print('After 1 vanilla')
-    pmf.Update('vanilla')
-    for hypo, prob in pmf.Items():
+    suite.Update('vanilla')
+    for hypo, prob in suite.Items():
         print(hypo, prob)
 
     print('\nAfter 1 vanilla, 1 chocolate')
-    pmf.Update('chocolate')
-    for hypo, prob in pmf.Items():
+    suite.Update('chocolate')
+    for hypo, prob in suite.Items():
+        print(hypo, prob)
+
+    print('\nAfter 1 vanilla, 1 chocolate, 1 vanilla')
+    suite.Update('vanilla')
+    for hypo, prob in suite.Items():
         print(hypo, prob)
 
 
