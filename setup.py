ec45b0642a1a62f1d4631b07f9c0c2bc940906b6
Allen Downey <downey@allendowney.com>
2018-08-25 12:39:52 -0400

    Creating a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ThinkBayes2',
+    version='2.0.0',
+    author='Allen B. Downey',
+    author_email='downey@allendowney.com',
+    packages=['thinkbayes2'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Supporting code for the book Think Bayes 2e.',
+    long_description=open('README.txt').read(),
+)
