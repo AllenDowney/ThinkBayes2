a6d938a10ea41e2163737965bb3ab18f8e6646cc
Allen Downey <downey@allendowney.com>
2020-05-16 16:27:32 -0400

    Updating chap02soln

--- code/soln/utils.py
+++ code/soln/utils.py
@@ -4,7 +4,23 @@ import matplotlib.pyplot as plt
 
 import re
 
+def write_table(table, label, **options):
+    """
+    """
+    filename = label + '.tex'
+    fp = open(filename, 'w')
+    s = table.to_latex(**options)
+    fp.write(s)
+    fp.close()
 
+def write_pmf(pmf, label):
+    """
+    """
+    df = pd.DataFrame()
+    df['qs'] = pmf.index
+    df['ps'] = pmf.values
+    write_table(df, label, index=False)
+    
 def underride(d, **options):
     """Add key-value pairs to d only if key is not in d.
 
@@ -36,3 +52,20 @@ def decorate(**options):
         ax.legend(handles, labels)
 
     plt.tight_layout()
+    
+    
+def savefig(root, **options):
+    """Save the current figure.
+    
+    root: string filename root
+    options: passed to plt.savefig
+    """
+    format = options.pop('format', None)
+    if format:
+        formats = [format]
+    else:
+        formats = ['pdf', 'png']
+
+    for format in formats:
+        fname = f'figs/{root}.{format}'
+        plt.savefig(fname, **options)
